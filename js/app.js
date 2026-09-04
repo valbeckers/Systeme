82773da7fe644aa235ae18a355e439f85fcbe45a
valbeckers <valbeck41@gmail.com>
2026-09-04 08:15:05 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ import {
   GRIMOIRE_ICON_DATA,
   DEBT_ACKNOWLEDGEMENT_ICON_DATA
 } from "./itemImages.js?v=20260808-items-normalized-v1";
-import { UiIcon } from "./uiIcons.js?v=20260903-pro-icons-v2";
+import { UiIcon } from "./uiIcons.js?v=20260904-pullups-bonus-v2";
 import { saveStoredState } from "./storage.js";
 import { cleanSystemState, exportSystemState } from "./stateSanitizer.js?v=20260823-portails-v1";
 import { buildInitialState, migrateGripsToMin } from "./stateBootstrap.js?v=20260903-objectifs-pro-v1";
